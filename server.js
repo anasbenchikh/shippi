03ad901fd5b958bac26dc5adaed910a93832215d
anasbenchikh <abenchikh67@gmail.com>
2025-11-28 02:36:21 +0100

    Fixing CORS Error Options

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const allowedOrigins = [
 
 app.use(cors({origin: allowedOrigins, credentials: true,
                 methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-                allowedHeaders: ['Content-Type', 'Authorization']}))
+                allowedHeaders: ['Content-Type', 'Authorization']}));
+app.options('*', cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
